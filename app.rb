57a0c6f9c1b6a4fe8db9fc7f7a9cde3b856fbcf2
hannako <jessjones1@gmail.com>
2016-07-19 12:32:08 +0100

    stage 13: adds form

--- app.rb
+++ app.rb
@@ -9,11 +9,12 @@ get '/secret' do
 end
 
 get '/random-cat' do
-	@name_sampler = ["Amigo", "Oscar", "Viking"].sample
+	@name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
+
 get '/named-cat' do
 	p params
-	@name_sampler = params[:name]
+	@name = params[:name]
   erb(:index)
 end
