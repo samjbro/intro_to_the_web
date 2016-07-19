d51080218ca4629afd3e53e6508ab664ef95be8a
samjbro <sjbroughton1@gmail.com>
2016-07-19 13:11:43 +0100

    Challenge 14: adds POST  method for submitting form

--- app.rb
+++ app.rb
@@ -13,8 +13,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
 	p params
 	@name = params[:name]
   erb(:index)
+end
+
+get '/cat-naming-form' do
+	erb(:form)
 end
